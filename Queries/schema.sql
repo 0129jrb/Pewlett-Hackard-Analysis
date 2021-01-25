bc9b53a25936700daf49f1c1bb9ce438da656a2b
Ruby Jiang <rubyjiang@Rubys-MacBook-Air.local>
2021-01-25 15:22:55 -0500

    message

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE departments ( dept_no VARCHAR(4) NOT NULL,
 						   dept_name VARCHAR(40) NOT NULL,
 						  PRIMARY KEY (dept_no),
 						  UNIQUE(dept_name) );
-CREATE TABLE employees (emp_no INT NOT NULL, 
+CREATE TABLE employees (emp_no INT NOT NULL,
 					    birth_date DATE NOT NULL,
 					    first_name VARCHAR NOT NULL,
 					    last_name VARCHAR NOT NULL,
@@ -11,7 +11,8 @@ CREATE TABLE employees (emp_no INT NOT NULL,
 					    hire_date DATE NOT NULL, PRIMARY KEY(emp_no));
 CREATE TABLE dept_manager(dept_no VARCHAR(4) NOT NULL,
 						 emp_no INT NOT NULL,from_date DATE NOT NULL,
-						 FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+						 to_date DATE NOT NULL,
+						  FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 						  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 						  PRIMARY KEY(emp_no, dept_no));
 CREATE TABLE salaries (emp_no INT NOT NULL,
@@ -21,17 +22,23 @@ CREATE TABLE salaries (emp_no INT NOT NULL,
                     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
                     PRIMARY KEY (emp_no));
 					
-CREATE TABLE dept_emp (dept_no VARCHAR(4) NOT NULL,
-					   emp_no INT NOT NULL, from_date DATE NOT NULL,
-					   to_date DATE NOT NULL, 
-					   FOREIGN KEY(emp_no) REFERENCES salaries(emp_no),
-					   PRIMARY KEY(dept_no));
-					   
-
-CREATE TABLE titles (emp_no INT NOT NULL,
-					 title INT NOT NULL, from_date DATE NOT NULL,
-					 to_date DATE NOT NULL,
-					 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-					 PRIMARY KEY(emp_no));
-SELECT* FROM departments;					 
-						  
+CREATE TABLE dept_emp (emp_no INT NOT NULL,
+					   dept_no VARCHAR(4) NOT NULL,
+					   from_date DATE NOT NULL,
+					   to_date DATE NOT NULL,
+					   FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+					   FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+					   PRIMARY KEY(emp_no,dept_no));
+					  
+CREATE TABLE "titles" (
+             "emp_no" INT   NOT NULL,
+             "title" VARCHAR   NOT NULL,
+             "from_date" DATE   NOT NULL,
+             "to_date" DATE   NOT NULL );
+			                                                
+SELECT* FROM departments;	
+SELECT* FROM employees;
+SELECT* FROM dept_manager;
+SELECT* FROM salaries;
+SELECT* FROM dept_emp;
+SELECT* FROM titles;
